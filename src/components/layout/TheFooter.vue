cbb95b8c89a722b2bd8033a2ed9837752ad1f62e
Grigor <ggrig93@gmail.com>
2022-05-11 18:29:00 +0400

    deleted subscribe

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -3,82 +3,82 @@
     <footer class="footer style7">
       <div class="container">
         <div class="container-wapper">
-          <div class="row">
-            <div class="box-footer col-xs-12 col-sm-4 col-md-4 col-lg-4 hidden-sm hidden-md hidden-lg">
-              <div class="ysera-newsletter style1">
-                <div class="newsletter-head">
-                  <h3 class="title">Newsletter</h3>
-                </div>
-                <div class="newsletter-form-wrap">
-                  <div class="list">
-                    Sign up for our free video course and <br/> urban garden inspiration
-                  </div>
-                  <input type="email" class="input-text email email-newsletter"
-                         placeholder="Your email letter">
-                  <button class="button btn-submit submit-newsletter">SUBSCRIBE</button>
-                </div>
-              </div>
-            </div>
-            <div class="box-footer col-xs-12 col-sm-4 col-md-4 col-lg-4">
-              <div class="ysera-custommenu default">
-                <h2 class="widgettitle">Quick Menu</h2>
-                <ul class="menu">
-                  <li class="menu-item">
-                    <a href="#">New arrivals</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="#">Life style</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="#">Bracelets</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="#">By Metal</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="#">Earrings</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="box-footer col-xs-12 col-sm-4 col-md-4 col-lg-4 hidden-xs">
-              <div class="ysera-newsletter style1">
-                <div class="newsletter-head">
-                  <h3 class="title">Newsletter</h3>
-                </div>
-                <div class="newsletter-form-wrap">
-                  <div class="list">
-                    Sign up for our free video course and <br/> urban garden inspiration
-                  </div>
-                  <input type="email" class="input-text email email-newsletter"
-                         placeholder="Your email letter">
-                  <button class="button btn-submit submit-newsletter">SUBSCRIBE</button>
-                </div>
-              </div>
-            </div>
-            <div class="box-footer col-xs-12 col-sm-4 col-md-4 col-lg-4">
-              <div class="ysera-custommenu default">
-                <h2 class="widgettitle">Information</h2>
-                <ul class="menu">
-                  <li class="menu-item">
-                    <a href="#">FAQs</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="#">Track Order</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="#">Delivery</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="#">Contact Us</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="#">Return</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+<!--          <div class="row">-->
+<!--            <div class="box-footer col-xs-12 col-sm-4 col-md-4 col-lg-4 hidden-sm hidden-md hidden-lg">-->
+<!--              <div class="ysera-newsletter style1">-->
+<!--                <div class="newsletter-head">-->
+<!--                  <h3 class="title">Newsletter</h3>-->
+<!--                </div>-->
+<!--                <div class="newsletter-form-wrap">-->
+<!--                  <div class="list">-->
+<!--                    Sign up for our free video course and <br/> urban garden inspiration-->
+<!--                  </div>-->
+<!--                  <input type="email" class="input-text email email-newsletter"-->
+<!--                         placeholder="Your email letter">-->
+<!--                  <button class="button btn-submit submit-newsletter">SUBSCRIBE</button>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--            <div class="box-footer col-xs-12 col-sm-4 col-md-4 col-lg-4">-->
+<!--              <div class="ysera-custommenu default">-->
+<!--                <h2 class="widgettitle">Quick Menu</h2>-->
+<!--                <ul class="menu">-->
+<!--                  <li class="menu-item">-->
+<!--                    <a href="#">New arrivals</a>-->
+<!--                  </li>-->
+<!--                  <li class="menu-item">-->
+<!--                    <a href="#">Life style</a>-->
+<!--                  </li>-->
+<!--                  <li class="menu-item">-->
+<!--                    <a href="#">Bracelets</a>-->
+<!--                  </li>-->
+<!--                  <li class="menu-item">-->
+<!--                    <a href="#">By Metal</a>-->
+<!--                  </li>-->
+<!--                  <li class="menu-item">-->
+<!--                    <a href="#">Earrings</a>-->
+<!--                  </li>-->
+<!--                </ul>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--            <div class="box-footer col-xs-12 col-sm-4 col-md-4 col-lg-4 hidden-xs">-->
+<!--              <div class="ysera-newsletter style1">-->
+<!--                <div class="newsletter-head">-->
+<!--                  <h3 class="title">Newsletter</h3>-->
+<!--                </div>-->
+<!--                <div class="newsletter-form-wrap">-->
+<!--                  <div class="list">-->
+<!--                    Sign up for our free video course and <br/> urban garden inspiration-->
+<!--                  </div>-->
+<!--                  <input type="email" class="input-text email email-newsletter"-->
+<!--                         placeholder="Your email letter">-->
+<!--                  <button class="button btn-submit submit-newsletter">SUBSCRIBE</button>-->
+<!--                </div>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--            <div class="box-footer col-xs-12 col-sm-4 col-md-4 col-lg-4">-->
+<!--              <div class="ysera-custommenu default">-->
+<!--                <h2 class="widgettitle">Information</h2>-->
+<!--                <ul class="menu">-->
+<!--                  <li class="menu-item">-->
+<!--                    <a href="#">FAQs</a>-->
+<!--                  </li>-->
+<!--                  <li class="menu-item">-->
+<!--                    <a href="#">Track Order</a>-->
+<!--                  </li>-->
+<!--                  <li class="menu-item">-->
+<!--                    <a href="#">Delivery</a>-->
+<!--                  </li>-->
+<!--                  <li class="menu-item">-->
+<!--                    <a href="#">Contact Us</a>-->
+<!--                  </li>-->
+<!--                  <li class="menu-item">-->
+<!--                    <a href="#">Return</a>-->
+<!--                  </li>-->
+<!--                </ul>-->
+<!--              </div>-->
+<!--            </div>-->
+<!--          </div>-->
           <div class="footer-end">
             <div class="row">
               <div class="col-sm-12 col-xs-12">
@@ -102,8 +102,8 @@
                   </ul>
                 </div>
                 <div class="coppyright">
-                  Copyright © 2020
-                  <a href="#">Ysera</a>
+                  Copyright © {{new Date().getFullYear()}}
+                  <router-link to="/">Ysera</router-link>
                   . All rights reserved
                 </div>
               </div>
